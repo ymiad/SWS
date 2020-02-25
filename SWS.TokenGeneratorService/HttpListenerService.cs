7a8df2eb4e2dd086ea9f7243644e397247715ba7
ymiad <ymiadzelets@gmail.com>
2020-02-25 23:36:08 +0300

    add TokenGeneratorService project

---
+++ SWS.TokenGeneratorService/HttpListenerService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.ServiceProcess;
+
+namespace SWS.TokenGeneratorService
+{
+    public partial class HttpListenerService : ServiceBase
+    {
+        private readonly HttpListener _listener;
+        private string _guid;
+
+        public HttpListenerService()
+        {
+            InitializeComponent();
+            _listener = new HttpListener();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _listener.Prefixes.Add("http://localhost:8888/");
+            _listener.Start();
+            _guid = Guid.NewGuid().ToString();
+
+            while (true)
+            {
+                var context = _listener.GetContext();
+                var response = context.Response;
+
+                if (string.IsNullOrEmpty(_guid))
+                {
+                    _guid = Guid.NewGuid().ToString();
+                }
+
+                var buffer = System.Text.Encoding.UTF8.GetBytes(_guid);
+                response.ContentLength64 = buffer.Length;
+                var output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _listener.Stop();
+        }
+    }
+}
---
+++ SWS.TokenGeneratorService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SWS.TokenGeneratorService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SWS.TokenGeneratorService")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2d535e5-cb72-486b-a1dd-dc0055031e43")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
