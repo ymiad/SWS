d23e6bbd8ae45e6d84e74f3e64c75bbe5cc1b70d
ymiad <ymiadzelets@gmail.com>
2020-02-25 23:56:31 +0300

    added token generation in service

---
+++ SWS.TokenGeneratorService/HttpListenerInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace SWS.TokenGeneratorService
+{
+    [RunInstaller(true)]
+    public partial class HttpListenerInstaller : System.Configuration.Install.Installer
+    {
+        public HttpListenerInstaller()
+        {
+            InitializeComponent();
+
+            var serviceInstaller = new ServiceInstaller();
+            var processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.NetworkService
+            };
+
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "SWT Token Generator Service";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
