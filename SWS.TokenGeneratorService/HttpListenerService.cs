bad2d163139430541b3aca1af7d8f257af015ddb
ymiad <ymiadzelets@gmail.com>
2020-05-02 22:28:13 +0300

    added request to server when card attached

--- SWS.TokenGeneratorService/HttpListenerService.cs
+++ SWS.TokenGeneratorService/HttpListenerService.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SWS.TokenGeneratorService
 {
@@ -17,25 +23,58 @@ namespace SWS.TokenGeneratorService
 
         protected override void OnStart(string[] args)
         {
+            StartHttpListener();
+        }
+
+        private async Task StartHttpListener()
+        {
             _listener.Prefixes.Add("http://localhost:8888/");
             _listener.Start();
             _guid = Guid.NewGuid().ToString();
 
             while (true)
             {
-                var context = _listener.GetContext();
-                var response = context.Response;
-
                 if (string.IsNullOrEmpty(_guid))
                 {
                     _guid = Guid.NewGuid().ToString();
                 }
 
-                var buffer = System.Text.Encoding.UTF8.GetBytes(_guid);
-                response.ContentLength64 = buffer.Length;
-                var output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+                var request = _listener.GetContext().Request;
+
+
+                if (request.QueryString.AllKeys.Contains("id"))
+                {
+                    await SendCardId(request.QueryString["id"]);
+                }
+                else
+                {
+                    var response = _listener.GetContext().Response;
+                    var buffer = Encoding.UTF8.GetBytes(_guid);
+                    response.ContentLength64 = buffer.Length;
+                    var output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    output.Close();
+                }
+            }
+        }
+
+        private async Task SendCardId(string cardId)
+        {
+            var client = new HttpClient();
+            var data = "{\"id\": \"" + cardId + "\", \"token\": \"" + _guid + "\"}";
+            var value = new Dictionary<string, string> {{"token", HashString(data)}};
+            var content = new FormUrlEncodedContent(value);
+
+            await client.PostAsync(Environment.GetEnvironmentVariable("SWSServerUrl"), content);
+        }
+
+        private static string HashString(string value)
+        {
+            using (var hash = SHA256.Create())
+            {
+                return string.Concat(hash
+                    .ComputeHash(Encoding.UTF8.GetBytes(value))
+                    .Select(item => item.ToString("x2")));
             }
         }
 
--- SWS.TokenGeneratorService/Program.cs
+++ SWS.TokenGeneratorService/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace SWS.TokenGeneratorService
 {
@@ -14,12 +9,11 @@ namespace SWS.TokenGeneratorService
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            var servicesToRun = new ServiceBase[]
             {
                 new HttpListenerService()
             };
-            ServiceBase.Run(ServicesToRun);
+            ServiceBase.Run(servicesToRun);
         }
     }
 }
